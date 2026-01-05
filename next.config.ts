71411b9fb93cf4fbe53a6b1c706e2396021822cb
navedsayyed <navedas9356@gmail.com>
2026-01-05 07:33:32 +0530

    Exclude sample context folder from build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // Ignore React Native sample files
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/sample context/**', '**/node_modules']
+    };
+    return config;
+  },
+  // Exclude sample context from compilation
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 };
 
 export default nextConfig;
