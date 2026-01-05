8e8f4869cdef1003054d10b45b5e0c68a7476016
navedsayyed <navedas9356@gmail.com>
2026-01-05 07:35:51 +0530

    Fix Turbopack config for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    // Ignore React Native sample files
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/sample context/**', '**/node_modules']
-    };
-    return config;
-  },
-  // Exclude sample context from compilation
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  turbopack: {}, // Empty config to silence the warning
 };
 
 export default nextConfig;
